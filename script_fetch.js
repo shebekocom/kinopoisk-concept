ea24466668db83dde340a065072a43b75d2aaafa
nshebeko <nikolay.shebeko@gmail.com>
2019-08-12 23:16:49 +0300

    add loading and img noposter

--- script_fetch.js
+++ script_fetch.js
@@ -2,15 +2,19 @@
 
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
+const urlPoster = 'https://image.tmdb.org/t/p/w500';
 
 function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=942cf276adefa306549de647ce5a6e18&language=ru&query=' + searchText;
-    movie.innerHTML = 'Загрузка';
+    movie.innerHTML = '<div class="spinner"></div>';
 
     fetch(server)
         .then(function(value){
+            if(value.status !==200){
+                return Promise.reject(new Error(value.status));
+            }
             return value.json();
         })
         .then(function(output){
@@ -18,17 +22,23 @@ function apiSearch(event) {
             output.results.forEach(function (item) {
                 let nameItem = item.name || item.title;
                 let dateItem = item.first_air_date || item.release_date;
-                inner += '<div class="col-5">' + nameItem + '</div>';
-                inner += '<div class="alert alert-primary" role="alert"> Дата релиза: ' + dateItem + '</div>';
+                const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
+                 inner += `
+                 <div class="col-12 col-md-6 col-xl-3 item">
+                 <img src="${poster}" class="img_poster" alt="${nameItem}">
+                 <p>${nameItem}</p>
+                    <div class="alert alert-primary" role="alert">
+                    Дата релиза: </br>${dateItem}
+                    </div>
+                 </div>`;
             });
             movie.innerHTML = inner;
         })
         .catch(function(reason){
             movie.innerHTML = 'Упс, что то пошо не так!';
-            console.log('error: ' + reason.status);
+            console.error('error: ' + reason);
         });
     
 }
 
 searchForm.addEventListener('submit', apiSearch);
-
