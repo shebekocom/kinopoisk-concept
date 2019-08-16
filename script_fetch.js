7409d7a53ce5e873a66939dd1ec3385e43c2ec06
nshebeko <nikolay.shebeko@gmail.com>
2019-08-17 01:10:56 +0300

    add informarion about movie

--- script_fetch.js
+++ script_fetch.js
@@ -11,10 +11,10 @@ function apiSearch(event) {
         movie.innerHTML = '<h2 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h2>';
         return
     }
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=942cf276adefa306549de647ce5a6e18&language=ru&query=' + searchText;
+
     movie.innerHTML = '<div class="spinner"></div>';
 
-    fetch(server)
+    fetch('https://api.themoviedb.org/3/search/multi?api_key=942cf276adefa306549de647ce5a6e18&language=ru&query=' + searchText)
         .then(function (value) {
             if (value.status !== 200) {
                 return Promise.reject(new Error(value.status));
@@ -64,7 +64,49 @@ function addEventMedia() {
 }
 
 function showFullInfo() {
-    console.log(this)
+    let url = '';
+    if (this.dataset.type === 'movie') {
+        url = 'https://api.themoviedb.org/3/movie/' +
+            this.dataset.id + '?api_key=942cf276adefa306549de647ce5a6e18&language=ru'
+    } else if (this.dataset.type === 'tv') {
+        url = 'https://api.themoviedb.org/3/tv/' +
+            this.dataset.id + '?api_key=942cf276adefa306549de647ce5a6e18&language=ru'
+    } else {
+        movie.innerHTML = '<h2 class="col-12 text-center text-info">Произошла ошибка перезайдите позже</h2>'
+    }
+
+    fetch(url)
+        .then(function (value) {
+            if (value.status !== 200) {
+                return Promise.reject(new Error(value.status));
+            }
+            return value.json();
+        })
+        .then(function (output) {
+            console.log(output)
+            movie.innerHTML = `
+            <h4 class="col-12 text-center text-info">${output.name || output.title}</h4>
+            <div class="col-4">
+                <img src='${urlPoster + output.poster_path}' alt='${output.name || output.title}'>
+                ${(output.homepage) ? `<p class='text-center'> <a href="${output.homepage}" target="_banck">Официальная страница</a></p>` : ''}
+                ${(output.imdb_id) ? `<p class='text-center'> <a href="https://imdb.com/title/${output.imdb_id}" target="_banck">Страницца на IMDB</a></p>` : ''}
+
+            </div>
+            <div class="col-8">
+                <p> Рейтинг: ${output.vote_average}</p>
+                <p> Статус: ${output.status}</p>
+                
+                ${(output.last_episode_to_air) ? `<p>${output.number_of_seasons} сезон ${output.last_episode_to_air.episode_number} серий вышло</p>` : ''}
+
+                <p> Описание: ${output.overview}</p>
+
+            </div>
+           `;
+        })
+        .catch(function (reason) {
+            movie.innerHTML = 'Упс, что то пошо не так!';
+            console.error(reason || reason.status);
+        });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -80,12 +122,13 @@ document.addEventListener('DOMContentLoaded', function () {
             if (output.results.length === 0) {
                 inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>';
             }
+
             output.results.forEach(function (item) {
                 let nameItem = item.name || item.title;
+                let mediaType = item.title ? 'movie' : 'tv';
                 let dateItem = item.first_air_date || item.release_date;
                 const poster = item.poster_path ? urlPoster + item.poster_path : './img/noposter.png';
-                let dataInfo = '';
-                if (item.media_type !== 'person') dataInfo = `data-id="${item.id}" data-type="${item.media_type}"`;
+                let dataInfo = `data-id="${item.id}" data-type="${mediaType}"`;
                 inner += `
                  <div class="col-12 col-md-6 col-xl-3 item">
                  <img src="${poster}" class="img_poster" alt="${nameItem}" ${dataInfo}>
